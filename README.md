d518be02ff117dafd72eff7f6057316038b47142
Dmitry Naidolinsky <naidv88@gmail.com>
2016-08-27 05:18:58 +0500

    новая версия, словари через файл настроек

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-translate - the translation of the text in the console by means of dictionaries of stardict.
+translate - the translation of the text in the console using dictionaries of stardict
 
-if the stardict.dict.dz dictionary then to execute gunzip stardict.dict.dz -c > stardict.dict
+In a house directory there shall be a file with the .translate.conf settings
+In this file it is possible to specify only two options.
 
-usage
-translate stardict.dict word
+jobdir = in this option can write folders where there will be dictionaries, it is possible to write several ways of folders through a comma, then search will be run in the listed folders. Search of dictionaries happens the recursive method, then it is possible to store several folders in the folder, without dumping all dictionaries in one folder.
 
-translate -  перевод текста в консоли посредством словарей stardict
+dicts = if this opition is commented out, means will be all dictionaries from the option jobdir will be checked. If the option dicts is included, then only the dictionaries listed in this option will be selected, it is possible to specify a little through a comma. There is an example.
 
-если словать stardict.dict.dz, тогда выполните gunzip stardict.dict.dz -c > stardict.dict
+jobdir=/home/user/dicts
+dicts=stardict.dictch
 
-использование
-translate stardict.dict слово
+When settings are complete, it is possible to use translate.
+translate word
--- main.c
+++ main.c
@@ -17,9 +17,24 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <ftw.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <asm-generic/ioctls.h>
+#include <sys/ioctl.h>
+
+#define DIR 1
+#define LIST 2
+
+struct configs {
+	        char *dirs;
+					        char *dicts;
+};
+
 const static char copying[]="<translate> Copyright (C) <2016> <Naidolinsy Dmitry>\n"
     "This program comes with ABSOLUTELY NO WARRANTY.\n"
 		"This is free software, and you are welcome to redistribute it\n"
@@ -30,22 +45,34 @@ const static char copying[]="<translate> Copyright (C) <2016> <Naidolinsy Dmitry
 
 void parser(char *,int);
 char *parserc;
+char *word;
 
-int main(int argc,const char *argv[])
+int nfile = 0;
+int print_file(char *file)
 {
-	if (argc < 3  ||  argc > 3){
-		printf("%s\n",copying);
-		printf("%s словарь [слово | фраза]\n",argv[0]);
-		return 1;
-	}
+	struct winsize ws;
+	ioctl(0,TIOCGWINSZ, &ws);
+	char *ptr;
+	char *line = calloc(ws.ws_col+ 14,sizeof(char));
+	memset(line,45,ws.ws_col + 14);
+	ptr = line;
+	ptr += ws.ws_col / 2;
+	char *cfile = calloc(strlen(file) , sizeof(char));
+	ptr = ptr - strlen(file) + (strlen(file) / 2);
+	sprintf(cfile,"\033[1;33m%s\033[1;36m",file);
+	strncpy(ptr,cfile,strlen(cfile));
+	printf("\033[1;36m%s\033[0m\n",line);
+
+}
+int process(char *file)
+{
 
 	FILE *dictionary;
-	if ((dictionary = fopen(argv[1],"r")) == NULL){
+	if ((dictionary = fopen(file,"r")) == NULL){
 		perror("fopen");
 		return 0;
 	}
 
-	char *word = argv[2];
 
 	char *target = calloc(128 ,sizeof(char));
 	sprintf(target, "<k>%s</k>",word);
@@ -60,6 +87,7 @@ int main(int argc,const char *argv[])
 	while (fgets(line, 1024,dictionary) != NULL){
 		if (found == 0)
 		if (ptr = strstr(line, target)){
+			print_file(file);
 			check = ptr;
 			found = 1;
 			if (( ptr = strstr(ptr, "<dtrn>"))==NULL){
@@ -254,4 +282,152 @@ skip:
 	}
 	printf("\033[0;37m");
 	printf("\n");
+}
+
+struct configs * getconfig();
+struct configs *cf;
+
+int found(const char *fpath, const struct stat *sb,
+		int typeflag, struct FTW *ftwbuf){
+	char *start;
+	char *end;
+	int symbol = 0;
+	if (cf->dicts != NULL){
+		end = cf->dicts;
+		start = end;
+		while(*end != '\0'){
+			end++;
+			if (symbol == 1)
+				if (!isspace(*end))
+					start = end,symbol=0;
+			if ((*end == ',') || (*end == '\0')){
+				char *name = calloc(end - start, sizeof(char));
+				strncpy(name,start,end - start);
+
+				if (strncmp(&fpath[ftwbuf->base],name,end - start)==0)
+					printf("%s\n",fpath);
+				free(name);
+				start = end+1;
+				symbol = 1;
+			}
+		}
+	}else
+		if (sb->st_mode & S_IFREG)
+		//printf("%s\n",fpath);
+		process(fpath);
+	
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2){
+		printf("\033[1m%s\033[0m",copying);
+		return 0;
+	}
+	word = argv[1];
+	cf = getconfig();
+
+	char *punct = cf->dirs;
+	char *start = punct;
+	char *currentdir;
+	int symbol = 0;
+	while(*punct != '\0'){
+		punct++;
+		if (symbol == 1)
+			if (!isspace(*punct))
+				start = punct,symbol=0;
+		if ((*punct == ',') || (*punct == '\0')){
+			currentdir = calloc(punct - start, sizeof(char));
+			strncpy(currentdir,start,punct - start);
+			nftw(currentdir, found,20, 0); 
+
+			if (*punct == '\0')
+				return 0;
+			punct += 1;
+			
+			start = punct;
+			if (isspace(*start))
+			symbol = 1;
+		}
+
+	}
+	return 0;
+}
+
+struct configs * getconfig()
+{
+	char *cpath = calloc(strlen(getenv("HOME")) + strlen(".translate.conf"),sizeof(char));
+	sprintf(cpath,"%s/.translate.conf",getenv("HOME"));
+
+	struct configs *cf = calloc(1,sizeof(struct configs));
+	FILE *conf;
+	if ((conf = fopen(cpath, "r"))==NULL){
+		perror("fopen");
+		return 0;
+	}
+	char line[255];
+	char *ptr;
+	int options=0;
+
+	while(fgets(line,254,conf)!=NULL){
+		if ((ptr = strstr(line,"jobdir")) && (line[0] != '#')){
+			if (isalpha(*(ptr + 6))){
+				int ind;
+				for (ind = 0; isalpha(*(ptr + ind));ind++);
+				*(ptr + ind)='\0';
+				fprintf(stderr,"\033[5;31mнеизвестная опция %s\n"
+											 "возможно вы имели ввиду jobdir\033[0m\n",ptr);
+				continue;
+			}
+			options += DIR;
+			if((ptr = strchr(line,'='))!=NULL)
+			if (*(ptr + 1) == '=')
+				fprintf(stderr,"\033[5;31mне может быть == в опции\033[0m\n");
+			ptr++;
+			for(;!isprint(*ptr);ptr++);
+			char *end = ptr;
+			for(;*end != '\n';end++);
+			cf->dirs = calloc(end - ptr + 2, sizeof(char));
+			strncpy(cf->dirs,ptr,end - ptr);
+			continue;
+		}
+		if ((ptr = strstr(line,"dicts")) && (line[0] != '#')){
+			if (isalpha(*(ptr + 5))){
+				int ind;
+				for (ind = 0; isalpha(*(ptr + ind));ind++);
+				*(ptr + ind)='\0';
+				fprintf(stderr,"\033[5;31mнеизвестная опция %s\n"
+											 "возможно вы имели ввиду dicts\033[0m\n",ptr);
+				continue;
+			}
+			options += LIST;
+			if((ptr = strchr(line,'='))!=NULL)
+			if (*(ptr + 1) == '=')
+				fprintf(stderr,"\033[5;31mне может быть == в опции\033[0m\n");
+			ptr++;
+			for(;!isprint(*ptr);ptr++);
+			char *end = ptr;
+			for(;*end != '\n';end++);
+			cf->dicts = calloc(end - ptr + 2, sizeof(char));
+			strncpy(cf->dicts,ptr,end - ptr);
+			continue;
+		}
+
+	}
+	fclose(conf);
+
+	if (options == 0){
+		/* нет настроек */
+	}else
+		if ( (options & DIR) && (options & LIST) ){
+			/* две опции */
+		}else
+			if (options & DIR){
+				/* все файлы в рабочем каталоге */
+			}else
+				if (options & LIST){
+					/* все перечисленные файлы */
+				}
+	return cf;
 }
